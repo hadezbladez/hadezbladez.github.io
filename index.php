418a56deee37873a484a5da8ae4415073535617d
hadezbladez <45381908+hadezbladez@users.noreply.github.com>
2019-03-10 16:17:59 +0700

    change and mistake
    html to php
    mistake ' and "

--- index.php
+++ index.php
@@ -4,8 +4,8 @@
   <body>
     <h1>Hello World. web Test</h1>
     <?php
-    echo 'I'm hosted with GitHub Pages.</br>';
-    echo 'This web is just a test there is nothing to see here...</br>';
+    echo "I'm hosted with GitHub Pages.</br>";
+    echo "This web is just a test there is nothing to see here...</br>";
     ?>
   </body>
   
